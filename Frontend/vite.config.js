b39d2a7b2620cc5af543b3a61d53981dd337fec5
Joshua <joshuavtk@hotmail.com>
2022-06-14 02:31:29 +0200

    Add service worker and basic offline functionality

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,14 +2,45 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        VitePWA({
+            includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+            manifest: {
+                name: 'Society of the Protection of Birds',
+                short_name: 'Short name of your app',
+                description: 'Description of your app',
+                theme_color: '#ffd100',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    }
+                ]
+            }
+        })
+    ],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
     base: "./",
+
 });
