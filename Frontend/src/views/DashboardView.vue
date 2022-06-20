edd3fcbe4d0b33ac1126bb6ab5d5e77e33e4a443
Chris Roscher <chris@aitai.nl>
2022-06-20 21:13:55 +0200

    Change chart to line chart

--- Frontend/src/views/DashboardView.vue
+++ Frontend/src/views/DashboardView.vue
@@ -1,93 +1,104 @@
+<!-- create chart with vue-chartjs -->
+
 <template>
-  <Bar
-    :chart-options="chartOptions"
-    :chart-data="chartData"
-    :chart-id="chartId"
-    :dataset-id-key="datasetIdKey"
-    :plugins="plugins"
-    :css-classes="cssClasses"
-    :styles="styles"
-    :width="width"
-    :height="height"
-  />
+  <div id="app">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h4 class="card-title">Wind speed vs temperature</h4>
+            </div>
+            <div class="card-body">
+              <canvas id="myChart" width="600" height="400"></canvas>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { Bar } from "vue-chartjs";
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale,
-} from "chart.js";
-
-ChartJS.register(
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale
-);
-
 export default {
-  name: "BarChart",
-  components: { Bar },
-  props: {
-    chartId: {
-      type: String,
-      default: "bar-chart",
-    },
-    datasetIdKey: {
-      type: String,
-      default: "label",
-    },
-    width: {
-      type: Number,
-      default: 400,
-    },
-    height: {
-      type: Number,
-      default: 400,
-    },
-    cssClasses: {
-      default: "",
-      type: String,
-    },
-    styles: {
-      type: Object,
-      default: () => {},
-    },
-    plugins: {
-      type: Object,
-      default: () => {},
-    },
-  },
   data() {
     return {
       chartData: {
-        labels: ["January", "February", "March"],
-        // get data from api
+        labels: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ],
         datasets: [
           {
-            label: "My First dataset",
-            backgroundColor: "rgba(0,99,132,0.2)",
-            data: [1, 10, 5],
+            label: "Wind Speed (km/h)",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(175,92,92,0.8)",
+            borderColor: "rgba(175,92,92,0.8)",
+            borderCapStyle: "butt",
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: "miter",
+            pointBorderColor: "rgba(175,92,92,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(175,92,92,1)",
+            pointHoverBorderColor: "rgba(175,92,92,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40, 35, 40, 39, 40, 40],
           },
           {
-            label: "My Second dataset",
-            backgroundColor: "rgba(255,99,132,0.2)",
-            data: [2, 12, 9],
+            label: "Temperature (°C)",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: "butt",
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: "miter",
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [35, 39, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40],
           },
         ],
       },
-      chartOptions: {
-        responsive: true,
-      },
     };
+  },
+  mounted() {
+    this.renderChart();
+  },
+  methods: {
+    renderChart() {
+      var ctx = document.getElementById("myChart").getContext("2d");
+      var myChart = new Chart(ctx, {
+        type: "line",
+        data: this.chartData,
+        options: {
+          responsive: true,
+        },
+      });
+    },
   },
 };
 </script>
